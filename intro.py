2557d078a8ca389ae139dde58babb30d33b06caa
savannahlawrence <savannah.lawrence@d11.org>
2024-09-23 17:26:24 +0000

    finished

--- intro.py
+++ intro.py
@@ -1,37 +1,38 @@
 # 1. Print "Hello World!" to the console
-
+print("Hello World!")
 
 
 # 2. Print "Hello my name is ______!" to the console
-
+print("Hello my name is Savannah!")
 
 
 # 3. Print "I love python!" to the console
-
+print("I love python!")
 
 
 # 4. Print your age to the console
-
+print(17)
 
 
 # 5. Print 5+5
-
+print(5+5)
 
 
 # 6. Print 4*3
-
+print(4*3)
 
 
 # 7. Print 50 - 40
-
+print(50-40)
 
 
 # 8. Print 100/5 and print 100//5. Make a comment explaining what the difference is.
-
-
-
+print(100/5)
+print(100//5)
+# the difference between 100/5 and 100//5 is whether or not the program runs it as an integer (whole number) or a floating number (decimal)
 # 9. Print 2**4
-
+print(2**4)
 
 
 # 10. Print 6*3-4+8//2
+print(6*3-4+8//2)
